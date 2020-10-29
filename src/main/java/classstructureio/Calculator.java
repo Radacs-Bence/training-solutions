7327ec797771fc6b07cef81f5859ba09c60758e8
Radacs-Bence <bence.radacs@gmail.com>
2020-10-29 12:35:34 +0100

    Seems finished

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-
+        System.out.println("Write two numbers with space between them:");
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = scanner.nextInt();
+        int secondNum = scanner.nextInt();
+        scanner.nextLine();
+        int sum = firstNum + secondNum;
+        System.out.println(firstNum + " + " + secondNum + "!");
+        System.out.println("(" + sum + ")!");
     }
 }

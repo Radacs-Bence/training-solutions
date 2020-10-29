445e8e3b572a8d06a12e12832df0216cf24750d4
Radacs-Bence <bence.radacs@gmail.com>
2020-10-29 13:20:01 +0100

    Second part of the task. "Seems finished" wasn't quite apt.

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,14 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        System.out.println("Név:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("E-mail:");
+        String eMail = scanner.nextLine();
+        System.out.println("Név:" + name + "\r\n" + "E-mail:" + eMail);
+    }
+}

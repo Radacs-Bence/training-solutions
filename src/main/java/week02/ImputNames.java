7373f3e53d9124d9986b3c37fb77c65f77e9e753
Radacs-Bence <bence.radacs@gmail.com>
2020-11-05 15:43:22 +0100

    Teams meeting Task

---
+++ src/main/java/week02/ImputNames.java
@@ -0,0 +1,29 @@
+package week02;
+
+import java.util.Scanner;
+
+public class ImputNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+
+        for (int i = 0; i < 5; i++){
+
+            String text;
+            if(i == 0){
+                text ="Aja meg az " + (i + 1) + ". nevet";
+            } else {
+                text ="Aja meg a " + (i + 1) + ". nevet";
+            }
+
+            System.out.println(text);
+            names[i] = scanner.nextLine();
+
+        }
+        for (int i = 0; i < 5; i++){
+            System.out.println((i + 1) + ". név:" + names[i]);
+        }
+    }
+}

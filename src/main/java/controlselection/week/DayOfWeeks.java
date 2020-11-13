d645df2053c6039262970d00d416a429743a76af
Radacs-Bence <bence.radacs@gmail.com>
2020-11-13 17:56:52 +0100

    Vezérlési szerkezetek és az elágazás task3

---
+++ src/main/java/controlselection/week/DayOfWeeks.java
@@ -0,0 +1,31 @@
+package controlselection.week;
+
+public class DayOfWeeks {
+
+    public String whichDay(String day){
+        String dayType;
+        switch(day.toLowerCase()){
+            case "hétfő":
+                dayType="hét eleje";
+                break;
+            case "kedd":
+            case "szerda":
+            case "csütörtök":
+                dayType="hét közepe";
+                break;
+            case "péntek":
+                dayType="majdnem hétvége";
+                break;
+            case "szombat":
+            case "vasárnap":
+                dayType="hét vége";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid day: " + day);
+        }
+
+        return dayType;
+
+    }
+
+}

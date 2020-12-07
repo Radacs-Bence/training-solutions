c682b4982012f33a5c26e38a2091c32920b68f5b
Radacs-Bence <bence.radacs@gmail.com>
2020-12-07 11:26:06 +0100

    Teams Meeting Task

---
+++ src/main/java/week07d01/Fibonacci.java
@@ -0,0 +1,20 @@
+package week07d01;
+
+public class Fibonacci {
+
+    public long fib(int n){
+        long resultOne = 1;
+        long resultTwo = 1;
+        for (int i = 2; i < n ; i++){
+            if (i % 2 == 0){
+                resultOne += resultTwo;
+            } else {
+                resultTwo += resultOne;
+            }
+        }
+        if (n % 2 == 0){
+            return resultTwo;
+        }
+        return resultOne;
+    }
+}

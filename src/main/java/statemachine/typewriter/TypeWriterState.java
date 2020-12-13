83ce3d4ad42388d55ff38d694380783a004d8f89
Radacs-Bence <bence.radacs@gmail.com>
2020-12-13 12:31:17 +0100

    Állapotgép task3

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,17 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+
+    UPPERCASE{
+        TypeWriterState next(){
+            return LOWERCASE;
+        }
+    },
+    LOWERCASE{
+        TypeWriterState next(){
+            return UPPERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}

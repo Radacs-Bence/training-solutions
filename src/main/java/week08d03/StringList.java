50440b5f2984f5680e1901d542289ebed12b3cd8
Radacs-Bence <bence.radacs@gmail.com>
2020-12-16 10:15:16 +0100

    Teams Meeting Task

---
+++ src/main/java/week08d03/StringList.java
@@ -0,0 +1,41 @@
+package week08d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringList {
+
+    public List<String> shortestWords(List<String> words){
+        valid(words);
+        int shortest =  shortestWordLength(words);
+        List<String> shortestWords = new ArrayList<>();
+        for (String word: words) {
+            if (word.length() == shortest){
+                shortestWords.add(word);
+            }
+        }
+        return shortestWords;
+    }
+
+    private int shortestWordLength(List<String> words){
+        int shortest = words.get(0).length();
+        for (String word: words) {
+            if (word.length() < shortest){
+                shortest = word.length();
+            }
+        }
+        return shortest;
+    }
+
+    private void valid(List<String> words){
+        if (words == null){
+            throw new NullPointerException("Word list cannot be null!");
+        }
+        for (String word: words) {
+            if (word == null){
+                throw new NullPointerException("Words in list cannot be null");
+            }
+        }
+    }
+
+}

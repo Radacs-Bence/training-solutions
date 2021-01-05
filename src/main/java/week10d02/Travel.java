67622869e1a970245f073c5da1ab03c47b387fb4
Radacs-Bence <bence.radacs@gmail.com>
2021-01-05 10:51:37 +0100

    Teams Meeting Task

--- src/main/java/week10d02/Travel.java
+++ src/main/java/week10d02/Travel.java
@@ -12,24 +12,37 @@ public class Travel {
 
     public int getStationWithMax(InputStream inputStream) {
         int maxStation = -1;
+        List<Integer> entries = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            int[] passengers = new int[30];
-            List<Integer> entries = new ArrayList<>();
             String line = "";
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
                 entries.add(Integer.parseInt(parts[0]));
             }
-            for (Integer number : entries) {
-                passengers[number]++;
-            }
-            for (int i = 0; i < 30; i++) {
-                if (passengers[i] > maxStation){
-                    maxStation = passengers[i];
-                }
-            }
         } catch (IOException ioException) {
             throw new IllegalStateException("Can not read File", ioException);
+        }
+        int[] passengers = counter(entries);
+        maxStation = max(passengers);
+        return maxStation;
+    }
+
+    private int[] counter(List<Integer> entries){
+        int[] passengers = new int[30];
+        for (Integer number : entries) {
+            passengers[number]++;
+        }
+        return passengers;
+    }
+
+    private int max (int[] passengers){
+        int maxStation = -1;
+        int maxPassengers = -1;
+        for (int i = 0; i < 30; i++) {
+            if (passengers[i] > maxPassengers){
+                maxPassengers = passengers[i];
+                maxStation = i;
+            }
         }
         return maxStation;
     }

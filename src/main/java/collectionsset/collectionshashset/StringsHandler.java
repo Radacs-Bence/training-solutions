600df73a489968597b6f8fbc2432e7dc0b574643
Radacs-Bence <bence.radacs@gmail.com>
2021-01-22 13:50:58 +0100

    Set task1

---
+++ src/main/java/collectionsset/collectionshashset/StringsHandler.java
@@ -0,0 +1,19 @@
+package collectionsset.collectionshashset;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringsHandler {
+
+    public Set<String> filterUniqueStrings(Collection<String> stringCollection) {
+        Set<String> uniques = new HashSet<>(stringCollection);
+        return uniques;
+    }
+
+    public Set<String> selectIdenticalStrings(Set<String> setA, Set<String> setB){
+        setA.retainAll(setB);
+        return setA;
+    }
+
+}

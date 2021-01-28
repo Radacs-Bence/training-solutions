5818a9fc14c9c4a15008bc6b8ca8e43ac645782f
Radacs-Bence <bence.radacs@gmail.com>
2021-01-28 10:33:54 +0100

    Teams Meeting Task

---
+++ src/main/java/week13d04/TemplateEngine.java
@@ -0,0 +1,49 @@
+package week13d04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class TemplateEngine {
+
+
+    public static final String KEY_START = "{";
+    public static final String KEY_END = "}";
+
+    public void merge(BufferedReader reader, Map<String, Object> toChange, BufferedWriter writer) {
+
+        try (reader) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String newLine = lineChanger(line, toChange);
+                writeText(newLine, writer);
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read file", e);
+        }
+
+    }
+
+    private String lineChanger(String line, Map<String, Object> toChange) {
+        String newLine =  "";
+        String key = "";
+        if (line.contains(KEY_START) && line.contains(KEY_END)){
+            key = line.substring(line.indexOf(KEY_START) + 1, line.indexOf(KEY_END));
+            newLine = line.replace(line.substring(line.indexOf(KEY_START), line.indexOf(KEY_END)+ 1), toChange.get(key).toString());
+        }
+        return newLine;
+    }
+
+    private void writeText(String newLine, BufferedWriter writer){
+        try(writer) {
+                writer.write(newLine);
+                writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot write file", e);
+        }
+    }
+
+
+}
